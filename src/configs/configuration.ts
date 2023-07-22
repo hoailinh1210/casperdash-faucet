a83b162dbbb5ba36e6de42ea7e627f2ba6cc1660
liam <hoailinh1210@gmail.com>
2023-07-22 09:36:22 +0700

    feat: add snc coin

--- src/configs/configuration.ts
+++ src/configs/configuration.ts
@@ -1,4 +1,6 @@
 export const Config = {
   cdContractHash:
     'hash-6902d991fed4a91076e5ff4246e116a7245e615d3683ecd83e794931d0527b17' as `hash-${string}`,
+  sncContractHash:
+    'hash-ea40ffd8b59dae927fcc82478c77e325cb6bf62f04627fe17e17d3ca615802da' as `hash-${string}`,
 };
